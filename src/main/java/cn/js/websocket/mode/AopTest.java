3be60b108ae8bc39d3aef6284e095f15816a72fa
tanping <tanping@globalegrow.com>
2018-08-18 09:56:36 +0800

    --- 添加 clear 日志功能

---
+++ src/main/java/cn/js/websocket/mode/AopTest.java
@@ -0,0 +1,18 @@
+/*
+package cn.js.websocket.mode;
+
+
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+//@EnableAspectJAutoProxy
+public class AopTest {
+
+   */
+/* @Before
+    public void test(){
+
+    }*//*
+
+}
+*/
